070a6b7bb6a436a6488ea034c89e69dff28b5aa7
Ewa Dwornik <ewaannadwornik@gmail.com>
2022-07-16 12:54:48 +0000

    Searching in articles

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -1,28 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../style/style.css';
 import {getAllArticles} from "../services/articles";
-import {Article} from "../model";
-
 
 // Page where you can read articles about herbs.
 function Articles() {
+    const [searchTerm, setSearchTerm] = useState("");
+    const handleChange = (event: any) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const results = getAllArticles().filter(article =>
+        (article.title).toLowerCase().includes(searchTerm) ||
+        (article.shortDescription).toLowerCase().includes(searchTerm)
+    );
 
     const articlesCard: any[] = [];
-    for (let article of getAllArticles()) {
+    for (let article of results) {
+
         articlesCard.push(
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <div className="card">
-                            <h5 className="card-header">{article.title}</h5>
+            <div>
+                <div className="card mb-3">
+                    <div className="row g-0 workshop-card">
+                        <div className="col-md-4 ">
+                            <img src={article.imageArtilces} className="img-fluid rounded-start"
+                                 alt={article.imageArtilces}/>
+                        </div>
+                        <div className="col-md-8">
                             <div className="card-body">
+                                <h5 className="card-title">{article.title}</h5>
                                 <p className="card-text">{article.shortDescription}</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
-                    </div>
-                    <div className="col">
-                        {article.imageArtilces}
+                        <div className="collapse " id={"article" + article.id}>
+                            <div className="card card-body long-description-article">
+                                {article.longDescription}
+                            </div>
+                        </div>
+                        <a href={"#article" + article.id} className="btn btn-article" data-bs-toggle="collapse"
+                           role="button" aria-expanded="false" aria-controls={"article" + article.id}>
+                            <span className="collapsed">
+                                Show more
+                            </span>
+                            <span className="expanded">
+                                Show less
+                            </span>
+                        </a>
                     </div>
                 </div>
             </div>
@@ -37,11 +59,23 @@ function Articles() {
                     labore et dolore magna aliqua. Lectus vestibulum mattis ullamcorper velit sed ullamcorper.
                     In
                     nibh mauris cursus mattis. Amet est placerat in egestas erat. Tristique senectus et netus et
-                    malesuada fames ac. Am et mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien.
+                    malesuada fames ac. Am et mauris commodo quis imperdiet massa tincidunt nunc pulvinar
+                    sapien.
                     Pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat. At tempor commodo
                     ullamcorper a lacus vestibulum sed arcu non.</p>
             </div>
             <div className="wider-grid">
+                <div className="row g-3 ">
+                    <div className="col-md-5">
+                        <input
+                            type="text"
+                            className="form-control searching-child searching-articles"
+                            placeholder="Search"
+                            value={searchTerm}
+                            onChange={handleChange}
+                        />
+                    </div>
+                </div>
                 <div>
                     {articlesCard}
                 </div>
@@ -51,6 +85,7 @@ function Articles() {
 }
 
 export default Articles;
+
 
 
 
